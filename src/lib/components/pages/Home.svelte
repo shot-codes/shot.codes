d86c84729fd93abab81b080aac49363dcddbdcfe
Nicolai <nicolai.verbaarschot@gmail.com>
2023-01-04 21:21:51 +0100

    enable orbit.

---
+++ src/lib/components/pages/Home.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { T, useTexture } from "@threlte/core";
+</script>
+
+<T.Mesh scale={1.0} position.y={0.4} position.x={0.3}>
+  <T.PlaneGeometry />
+  <T.MeshBasicMaterial map={useTexture("/xp-limo.png")} />
+</T.Mesh>

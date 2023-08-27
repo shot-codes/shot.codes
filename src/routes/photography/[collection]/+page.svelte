ac53c3198a29bcb79df2f3dc88d75661b1e7e749
Nicolai <nicolai.verbaarschot@gmail.com>
2023-08-28 00:00:11 +0200

    added title to show that page is indeed changing.

--- src/routes/photography/[collection]/+page.svelte
+++ src/routes/photography/[collection]/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import type { ImageSet as ImageSetType } from 'web-image-gen-svelte';
 	import ImageSet from 'web-image-gen-svelte';
 
 	export let data: PageData;
 </script>
+
+<h1 class="absolute right-0 top-0">{data.collection}</h1>
 
 <div class="mt-[200px] flex flex-col">
 	{#each Object.values(data.collectionConfig) as set}

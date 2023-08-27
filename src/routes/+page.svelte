237fcb80d8365860b6d62e87fbc190b66d9a127c
Nicolai <nicolai.verbaarschot@gmail.com>
2023-08-27 22:31:38 +0200

    rotated links.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
-<div class="flex h-full items-end px-10 pb-[100px]">
-	<div class="w-full">
-		<div class="flex h-full w-full flex-wrap items-end justify-center space-y-2 font-sans">
-			<a href="photography" class="mx-2 hover:underline">PHOTOGRAPHY</a>
-			<a href="projects" class="mx-2 hover:underline">PROJECTS</a>
-			<a href="games" class="mx-2 hover:underline">GAMES</a>
-			<a href="blog" class="mx-2 hover:underline">BLOG</a>
-		</div>
-	</div>
-</div>
+<a
+	href="photography"
+	class="fixed bottom-[110px] right-[190px] mx-2 rotate-45 transform hover:underline">PHOTOGRAPHY</a
+>
+<a
+	href="projects"
+	class="fixed bottom-[98px] right-[140px] mx-2 rotate-45 transform hover:underline">PROJECTS</a
+>
+<a href="games" class="fixed bottom-[88px] right-[88px] mx-2 rotate-45 transform hover:underline"
+	>GAMES</a
+>
+<a href="blog" class="fixed bottom-[84px] right-[35px] mx-2 rotate-45 transform hover:underline"
+	>BLOG</a
+>

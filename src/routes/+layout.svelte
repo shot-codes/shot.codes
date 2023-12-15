d1220f1507a285e8a2fc2c860db86230627616b8
shot-codes <nicolai.verbaarschot@gmail.com>
2023-12-15 01:51:07 +0100

    Added favicons.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,13 @@
 
 <svelte:head>
 	<meta name="homepage" />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+	<link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+
 	<style>
 		html {
 			background-color: var(--bg-home);
